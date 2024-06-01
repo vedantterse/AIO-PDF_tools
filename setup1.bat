ab45c8f880085dfbe2462bf5be90f45233fa7c63
vedantterse <vedantterse75575@gmail.com>
2024-06-01 17:29:22 +0530

    setup file

--- setup1.bat
+++ setup1.bat
@@ -1,35 +1,15 @@
 @echo off
 echo Setting up PDF Tools...
 
-:: Function to create directories
-:CREATE_DIRECTORIES
-for %%i in (
-    "PDF_Merger"
-    "Merged_pdf"
-    "input_img2pdf"
-    "img2pdf_output"
-    "PDF_ENCRYPT"
-    "ENCRYPTED_PDF"
-    "input_pdf_to_img"
-    "output_pdf_to_img"
-    "input_split"
-    "output_split"
-    "input_custom_pages"
-    "output_custom_pages"
-) do (
-    if not exist "%%~i" (
-        mkdir "%%~i"
-        echo Created directory: %%~i
-    )
-)
-
 :: Function to install dependencies
 :INSTALL_DEPENDENCIES
 echo Installing dependencies...
-pip install PyPDF2
-pip install fitz
-pip install img2pdf
+
+:: Install dependencies from requirements.txt
+pip install -r requirements.txt
+
 echo Dependencies installed.
+
 
 echo Setup complete.
 pause
